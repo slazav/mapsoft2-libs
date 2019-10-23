741c920ccc3826f7cca63b5ee58eb12844dfc9bb
Vladislav Zavjalov <slazav@altlinux.org>
2019-10-23 11:46:42 +0300

    modules/mapview: add group and step tabs in the mapdb panel. Switching of group/step visibility

--- mapview/panel_mapdb.h
+++ mapview/panel_mapdb.h
@@ -7,48 +7,126 @@
 /* Control panel for MapDB.
 */
 
-class PanelMapDBRecord : public Gtk::TreeModelColumnRecord {
+class MapDBGrRecord : public Gtk::TreeModelColumnRecord {
 public:
     Gtk::TreeModelColumn<bool> checked;
-    Gtk::TreeModelColumn<std::string> step;
+    Gtk::TreeModelColumn<std::string> name;
 
-    PanelMapDBRecord() {
-      add(checked); add(step);
+    MapDBGrRecord() {
+      add(checked); add(name);
+    }
+};
+
+class MapDBStRecord : public Gtk::TreeModelColumnRecord {
+public:
+    Gtk::TreeModelColumn<bool> checked;
+    Gtk::TreeModelColumn<std::string> name;
+    Gtk::TreeModelColumn<std::shared_ptr<GObj> > obj;
+
+    MapDBStRecord() {
+      add(checked); add(name); add(obj);
     }
 };
 
 /**********************************************************/
 
-class PanelMapDB : public Gtk::ScrolledWindow, public GObjMapDB {
-  Gtk::TreeView *treeview;
-  Glib::RefPtr<Gtk::ListStore> store;
-  PanelMapDBRecord columns;
+class PanelMapDB : public Gtk::Notebook, public GObjMapDB {
+  Gtk::TreeView *tv_gr, *tv_st; // list of groups and steps
+  Glib::RefPtr<Gtk::ListStore> store_gr, store_st;
+  MapDBGrRecord cols_gr;
+  MapDBStRecord cols_st;
 
 public:
   PanelMapDB(const std::string & mapdir):
       GObjMapDB(mapdir) {
 
-    treeview = manage(new Gtk::TreeView);
-    store = Gtk::ListStore::create(columns);
-    treeview->set_name("VMAP");
-    treeview->set_model(store);
-    treeview->append_column_editable("V", columns.checked);
-    treeview->append_column_editable("V", columns.step);
+    /*******************************/
+    // Setup group view
 
-    store->signal_row_changed().connect (
-      sigc::mem_fun (this, &PanelMapDB::on_panel_edited));
+    tv_gr = manage(new Gtk::TreeView);
+    store_gr = Gtk::ListStore::create(cols_gr);
+    tv_gr->set_name("MapDB groups");
+    tv_gr->set_model(store_gr);
+    tv_gr->append_column_editable("V", cols_gr.checked);
+    tv_gr->append_column("V", cols_gr.name);
 
-    treeview->set_enable_search(false);
-    treeview->set_headers_visible(false);
-    treeview->set_reorderable(false);
 
-    set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
-    Gtk::ScrolledWindow::add(*treeview);
+    tv_gr->set_enable_search(false);
+    tv_gr->set_headers_visible(false);
+    tv_gr->set_reorderable(false);
+
+    // pack into scroll window
+    Gtk::ScrolledWindow * scr_gr = manage(new Gtk::ScrolledWindow);
+    scr_gr->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    scr_gr->add(*tv_gr);
+
+    /*******************************/
+    // Setup step view
+    tv_st = manage(new Gtk::TreeView);
+    store_st = Gtk::ListStore::create(cols_st);
+    tv_st->set_name("MapDB groups");
+    tv_st->set_model(store_st);
+    tv_st->append_column_editable("V", cols_st.checked);
+    tv_st->append_column("V", cols_st.name);
+
+    tv_st->set_enable_search(false);
+    tv_st->set_headers_visible(false);
+    tv_st->set_reorderable(false);
+
+    // pack into scroll window
+    Gtk::ScrolledWindow * scr_st = manage(new Gtk::ScrolledWindow);
+    scr_st->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    scr_st->add(*tv_st);
+
+    /*******************************/
+    // Setup the main widget (Gtk::Notebook)
+    set_name("MapDB viewes");
+    set_scrollable(false);
+    //set_size_request(100,-1);
+    append_page(*scr_gr, "Groups", "Groups");
+    append_page(*scr_st, "Steps", "Steps");
+
+    /*******************************/
+    // add data
+
+    for (auto const & st:GObjMapDB::get_data()){
+      auto it = store_st->append();
+      Gtk::TreeModel::Row row = *it;
+      row[cols_st.checked] = get_visibility(st);
+      row[cols_st.name]    = ((DrawingStep*)st.get())->get_name();
+      row[cols_st.obj]     = st;
+    }
+
+    for (auto const & gr:get_groups()){
+      auto it = store_gr->append();
+      Gtk::TreeModel::Row row = *it;
+      row[cols_gr.checked] = true;
+      row[cols_gr.name]    = gr;
+    }
+
+    // connect signals
+    store_gr->signal_row_changed().connect (
+      sigc::mem_fun (this, &PanelMapDB::on_gr_edited));
+    store_st->signal_row_changed().connect (
+      sigc::mem_fun (this, &PanelMapDB::on_st_edited));
+
   }
 
   // callback for updating data from the panel
-  void on_panel_edited (const Gtk::TreeModel::Path& path,
-                        const Gtk::TreeModel::iterator& iter) {
+  void on_gr_edited (const Gtk::TreeModel::Path& path,
+                     const Gtk::TreeModel::iterator& iter) {
+    set_group_visibility(
+      (*iter)[cols_gr.name],
+      (*iter)[cols_gr.checked]
+    );
+  }
+
+  void on_st_edited (const Gtk::TreeModel::Path& path,
+                     const Gtk::TreeModel::iterator& iter) {
+    set_visibility(
+      (*iter)[cols_st.obj],
+      (*iter)[cols_st.checked]
+    );
   }
 
 };
