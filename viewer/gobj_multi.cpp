a0b58558d8c7f63f9015d51d6cafdd0c0f310430
Vladislav Zavjalov <slazav@altlinux.org>
2019-10-23 15:20:25 +0300

    modules/viewer: move code from gobj_multi.h to .cpp

---
+++ viewer/gobj_multi.cpp
@@ -0,0 +1,164 @@
+#include "gobj_multi.h"
+
+#include <map>
+#include <vector>
+#include <memory>
+#include "gobj.h"
+
+
+std::multimap<int, GObjMulti::GObjData>::iterator
+GObjMulti::find(const std::shared_ptr<GObj> & o) {
+  for (auto i = data.begin(); i!=data.end(); ++i)
+    if (i->second.obj == o) return i;
+  return data.end();
+}
+
+std::multimap<int, GObjMulti::GObjData>::const_iterator
+GObjMulti::find(const std::shared_ptr<GObj> & o) const {
+  for (auto i = data.begin(); i!=data.end(); ++i)
+    if (i->second.obj == o) return i;
+  return data.end();
+}
+
+/************************************************/
+// Public methods
+
+void
+GObjMulti::add(int depth, std::shared_ptr<GObj> o){
+  if (!o) return;
+  o->set_opt(opt);
+  o->set_cnv(cnv);
+
+  stop_drawing = true;
+  auto lock = get_lock();
+
+  GObjData D;
+  D.obj = o;
+  D.on = true;
+  auto & sig = signal_redraw_me();
+  D.redraw_conn = o->signal_redraw_me().connect(
+    sigc::mem_fun (&sig, &sigc::signal<void, iRect>::emit));
+  data.emplace(-depth, D); // we use negative depth for correct sorting
+
+  stop_drawing = false;
+  signal_redraw_me().emit(iRect());
+}
+
+std::vector<std::shared_ptr<GObj> >
+GObjMulti::get_data() const {
+  std::vector<std::shared_ptr<GObj> > ret;
+  for (auto const & d:data)
+    ret.push_back(d.second.obj);
+  return ret;
+}
+
+int
+GObjMulti::get_depth(std::shared_ptr<GObj> o) const{
+  auto it = find(o);
+  if (it==data.end())
+    throw Err() << "GObjMulti::get_depth: no such object";
+  return -it->first;
+}
+
+bool
+GObjMulti::get_visibility(std::shared_ptr<GObj> o) const{
+  auto it = find(o);
+  if (it==data.end())
+    throw Err() << "GObjMulti::get_visibility: no such object";
+  return it->second.on;
+}
+
+void
+GObjMulti::set_depth(std::shared_ptr<GObj> o, int depth){
+  if (!o) return;
+
+  stop_drawing = true;
+  auto lock = get_lock();
+
+  auto it = find(o);
+  if (it==data.end()) return;
+  if (it->first == -depth) return;
+  data.emplace(-depth, it->second);
+  data.erase(it);
+
+  stop_drawing = false;
+  signal_redraw_me().emit(iRect());
+}
+
+void
+GObjMulti::set_visibility(std::shared_ptr<GObj> o, bool on){
+  if (!o) return;
+
+  stop_drawing = true;
+  auto lock = get_lock();
+
+  auto it = find(o);
+  if (it==data.end()) return;
+  it->second.on = on;
+
+  stop_drawing = false;
+  signal_redraw_me().emit(iRect());
+}
+
+void
+GObjMulti::del(std::shared_ptr<GObj> o){
+  if (!o) return;
+
+  stop_drawing = true;
+  auto lock = get_lock();
+
+  auto it = find(o);
+  if (it==data.end()) return;
+  it->second.redraw_conn.disconnect();
+  data.erase(it);
+
+  stop_drawing = false;
+  signal_redraw_me().emit(iRect());
+}
+
+void
+GObjMulti::clear(){
+  stop_drawing = true;
+  auto lock = get_lock();
+
+  for (auto & o:data) o.second.redraw_conn.disconnect();
+  data.clear();
+
+  stop_drawing = false;
+  signal_redraw_me().emit(iRect());
+}
+
+/************************************************/
+// override default GObj methods
+
+int
+GObjMulti::draw(const CairoWrapper & cr, const dRect & draw_range){
+  int res = GObj::FILL_NONE;
+  for (auto const & p:data){
+    if (!p.second.on) continue;
+    cr->save();
+    int res1 = p.second.obj->draw(cr, draw_range);
+    cr->restore();
+    if (res1 != GObj::FILL_NONE &&
+        res!=GObj::FILL_ALL) res=res1;
+  }
+  return res;
+}
+
+void
+GObjMulti::on_rescale(double k){
+  // Note that `on_rescale` is called instead on `rescale`.
+  // Thus cnv is not modified, locking is not done, signal_redraw_me
+  // is not emitted in sub-objects, it is done in GobjMulti::rescale!
+  for (auto const & p:data) p.second.obj->on_rescale(k);
+}
+
+void
+GObjMulti::on_set_cnv(){
+  for (auto const & p:data) p.second.obj->set_cnv(cnv);
+}
+
+void
+GObjMulti::on_set_opt(){
+  for (auto const & p:data) p.second.obj->set_opt(opt);
+}
--- viewer/gobj_multi.h
+++ viewer/gobj_multi.h
@@ -20,167 +20,56 @@ private:
 
   std::multimap<int, GObjData> data;
 
-public:
-
-  // Add new object at some depth (larger depth - earlier the object is drawn)
-  void add(int depth, std::shared_ptr<GObj> o){
-    if (!o) return;
-    o->set_opt(opt);
-    o->set_cnv(cnv);
-
-    stop_drawing = true;
-    auto lock = get_lock();
-
-    GObjData D;
-    D.obj = o;
-    D.on = true;
-    auto & sig = signal_redraw_me();
-    D.redraw_conn = o->signal_redraw_me().connect(
-      sigc::mem_fun (&sig, &sigc::signal<void, iRect>::emit));
-    data.emplace(-depth, D); // we use negative depth for correct sorting
-
-    stop_drawing = false;
-    signal_redraw_me().emit(iRect());
-  }
-
-  // size(), number of objects
-  int size() const { return data.size(); }
-
-private:
-
   // find an object
   std::multimap<int, GObjData>::iterator
-  find(const std::shared_ptr<GObj> & o) {
-    for (auto i = data.begin(); i!=data.end(); ++i)
-      if (i->second.obj == o) return i;
-    return data.end();
-  }
+    find(const std::shared_ptr<GObj> & o);
 
   // find an object (const version)
   std::multimap<int, GObjData>::const_iterator
-  find(const std::shared_ptr<GObj> & o) const {
-    for (auto i = data.begin(); i!=data.end(); ++i)
-      if (i->second.obj == o) return i;
-    return data.end();
-  }
+    find(const std::shared_ptr<GObj> & o) const;
 
 public:
 
+  // Add new object at some depth (larger depth - earlier the object is drawn)
+  void add(int depth, std::shared_ptr<GObj> o);
+
+  // Return number of objects
+  int size() const { return data.size(); }
+
   // get list of all objects
-  std::vector<std::shared_ptr<GObj> > get_data() const {
-    std::vector<std::shared_ptr<GObj> > ret;
-    for (auto const & d:data)
-      ret.push_back(d.second.obj);
-    return ret;
-  }
+  std::vector<std::shared_ptr<GObj> > get_data() const;
 
   // get object depth (-1 if there is no such object)
-  int get_depth(std::shared_ptr<GObj> o) const{
-    auto it = find(o);
-    if (it==data.end())
-      throw Err() << "GObjMulti::get_depth: no such object";
-    return -it->first;
-  }
+  int get_depth(std::shared_ptr<GObj> o) const;
 
   // get object visibility trow Err if there is no such object)
-  bool get_visibility(std::shared_ptr<GObj> o) const{
-    auto it = find(o);
-    if (it==data.end())
-      throw Err() << "GObjMulti::get_visibility: no such object";
-    return it->second.on;
-  }
+  bool get_visibility(std::shared_ptr<GObj> o) const;
 
   // set object depth
-  void set_depth(std::shared_ptr<GObj> o, int depth){
-    if (!o) return;
-
-    stop_drawing = true;
-    auto lock = get_lock();
-
-    auto it = find(o);
-    if (it==data.end()) return;
-    if (it->first == -depth) return;
-    data.emplace(-depth, it->second);
-    data.erase(it);
-
-    stop_drawing = false;
-    signal_redraw_me().emit(iRect());
-  }
+  void set_depth(std::shared_ptr<GObj> o, int depth);
 
   // set object visibility
-  void set_visibility(std::shared_ptr<GObj> o, bool on){
-    if (!o) return;
-
-    stop_drawing = true;
-    auto lock = get_lock();
-
-    auto it = find(o);
-    if (it==data.end()) return;
-    it->second.on = on;
-
-    stop_drawing = false;
-    signal_redraw_me().emit(iRect());
-  }
+  void set_visibility(std::shared_ptr<GObj> o, bool on);
 
   // delete an object
-  void del(std::shared_ptr<GObj> o){
-    if (!o) return;
-
-    stop_drawing = true;
-    auto lock = get_lock();
-
-    auto it = find(o);
-    if (it==data.end()) return;
-    it->second.redraw_conn.disconnect();
-    data.erase(it);
-
-    stop_drawing = false;
-    signal_redraw_me().emit(iRect());
-  }
+  void del(std::shared_ptr<GObj> o);
 
   // delete all objects
-  void clear(){
-    stop_drawing = true;
-    auto lock = get_lock();
+  void clear();
 
-    for (auto & o:data) o.second.redraw_conn.disconnect();
-    data.clear();
-
-    stop_drawing = false;
-    signal_redraw_me().emit(iRect());
-  }
+  // Override GObj methods
 
   // Draw all objects
-  int draw(const CairoWrapper & cr, const dRect & draw_range) override{
-    int res = GObj::FILL_NONE;
-    for (auto const & p:data){
-      if (!p.second.on) continue;
-      cr->save();
-      int res1 = p.second.obj->draw(cr, draw_range);
-      cr->restore();
-      if (res1 != GObj::FILL_NONE &&
-          res!=GObj::FILL_ALL) res=res1;
-    }
-    return res;
-  }
+  int draw(const CairoWrapper & cr, const dRect & draw_range) override;
 
   // Rescale
-  void on_rescale(double k) override{
-    // Note that `on_rescale` is called instead on `rescale`.
-    // Thus cnv is not modified, locking is not done, signal_redraw_me
-    // is not emitted in sub-objects, it is done in GobjMulti::rescale!
-    for (auto const & p:data) p.second.obj->on_rescale(k);
-  }
+  void on_rescale(double k) override;
 
   // Set cnv
-  void on_set_cnv() override{
-    for (auto const & p:data) p.second.obj->set_cnv(cnv);
-  }
+  void on_set_cnv() override;
 
   // Set options
-  void on_set_opt() override{
-    for (auto const & p:data) p.second.obj->set_opt(opt);
-  }
+  void on_set_opt() override;
 
 };
 
