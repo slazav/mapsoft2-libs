ffe20b8ca3c89a73174c09f6b763c1869b441d73
Vladislav Zavjalov <slazav@altlinux.org>
2019-11-02 20:58:09 +0200

    modules/mapdb: fix VMAP export, add more options, tests

--- mapdb/mapdb_vmap.cpp
+++ mapdb/mapdb_vmap.cpp
@@ -10,12 +10,55 @@
 
 #include "mapdb.h"
 
-using namespace std;
-
+/**********************************************************/
 /* Import/export of old mapsoft VMAP format */
 
+using namespace std;
+
 /**********************************************************/
-/// Import objects from VMAP1 file.
+void
+ms2opt_add_mapdb_vmap_imp(GetOptSet & opts){
+  const char *g = "MAPDB_VMAP_IMP";
+  opts.add("config", 1,'c',g,
+    "Configuration file for VMAP->MapDB conversion");
+
+  opts.add("cnv_points", 1,0,g,
+    "Type conversion rules for point objects: "
+    "[[<type_in>, <type_out>], [<type_in>, <type_out>], ...] "
+    "type_in==0 matches all types; type_out==0 sets output type "
+    "same as input one. Default value: [[0,0]].");
+
+  opts.add("cnv_lines", 1,0,g,
+    "Type conversion rules for line objects. Same notation as in cnv_point.");
+
+  opts.add("cnv_areas", 1,0,g,
+    "Type conversion rules for area objects. Same notation as in cnv_point.");
+
+}
+
+/**********************************************************/
+void
+ms2opt_add_mapdb_vmap_exp(GetOptSet & opts){
+  const char *g = "MAPDB_VMAP_EXP";
+  opts.add("config", 1,'c',g,
+    "Configuration file for MapDB->VMAP conversion");
+
+ opts.add("cnv_points", 1,0,g,
+    "Type conversion rules for point objects: "
+    "[[<type_in>, <type_out>], [<type_in>, <type_out>], ...] "
+    "type_in==0 matches all types; type_out==0 sets output type "
+    "same as input one. Default value: [[0,0]].");
+
+  opts.add("cnv_lines", 1,0,g,
+    "Type conversion rules for line objects. Same notation as in cnv_point.");
+
+  opts.add("cnv_areas", 1,0,g,
+    "Type conversion rules for area objects. Same notation as in cnv_point.");
+}
+
+
+/**********************************************************/
+/// Import objects from VMAP file.
 void
 MapDB::import_vmap(const std::string & vmap_file, const Opt & opts){
 
@@ -29,7 +72,6 @@ MapDB::import_vmap(const std::string & vmap_file, const Opt & opts){
   // data level
   int level = 0;
 
-
   // Read configuration file.
   if (opts.exists("config")){
     int line_num[2] = {0,0};
@@ -37,31 +79,52 @@ MapDB::import_vmap(const std::string & vmap_file, const Opt & opts){
     // reset default configuration:
     for (int i=0; i<3; i++) cnvs[i] = iLine();
 
+
     ifstream ff(opts.get<string>("config"));
-    while (1){
-      vector<string> vs = read_words(ff, line_num, true);
-      if (vs.size()<1) continue;
+    try {
+      while (1){
+        vector<string> vs = read_words(ff, line_num, true);
+        if (vs.size()<1) break;
 
-      int cl = -1;
-      if (vs[0]=="point" || vs[0]=="poi") cl=0;
-      if (vs[0]=="line" || vs[0]=="multiline") cl=1;
-      if (vs[0]=="polygon") cl=2;
+        if (vs[0]=="point"){
+          if (vs.size()!=2 && vs.size()!=3)
+            throw Err() << "point: two or three arguments expected: "
+                           "<type_in> [<type_out>]";
+          cnvs[0].push_back(iPoint(
+            str_to_type<int>(vs[1]), vs.size()<3? 0:str_to_type<uint16_t>(vs[2])));
+          continue;
+        }
 
-      if (cl>=0 &&  vs.size()>1 && vs.size()<3){
-        cnvs[cl].push_back(iPoint(
-          str_to_type<int>(vs[1]),
-          vs.size()<3? 0:str_to_type<int>(vs[2])));
-        continue;
+        if (vs[0]=="line"){
+          if (vs.size()!=2 && vs.size()!=3)
+            throw Err() << "line: two or three arguments expected: "
+                           "<type_in> [<type_out>]";
+          cnvs[1].push_back(iPoint(
+            str_to_type<int>(vs[1]), vs.size()<3? 0:str_to_type<uint16_t>(vs[2])));
+          continue;
+        }
+
+        if (vs[0]=="area"){
+          if (vs.size()!=2 && vs.size()!=3)
+            throw Err() << "area: two or three arguments expected: "
+                           "<type_in> [<type_out>]";
+          cnvs[2].push_back(iPoint(
+            str_to_type<int>(vs[1]), vs.size()<3? 0:str_to_type<uint16_t>(vs[2])));
+          continue;
+        }
+
+        throw Err() << "unknown command: " << vs[0];
       }
-
-      throw Err() << "bad configuration file at line "
-                  << line_num[0];
+    } catch (Err e){
+        throw Err() << "MapDB::import_mp: bad configuration file at line "
+                    << line_num[0] << ": " << e.str();
     }
   }
 
-  if (opts.exists("cnv_point"))   cnvs[0] = opts.get<dLine>("cnv_point");
-  if (opts.exists("cnv_line"))    cnvs[0] = opts.get<dLine>("cnv_line");
-  if (opts.exists("cnv_polygon")) cnvs[0] = opts.get<dLine>("cnv_polygon");
+  if (opts.exists("cnv_points")) cnvs[0] = opts.get<dLine>("cnv_points");
+  if (opts.exists("cnv_lines"))  cnvs[1] = opts.get<dLine>("cnv_lines");
+  if (opts.exists("cnv_areas"))  cnvs[2] = opts.get<dLine>("cnv_areas");
+
 
   // read VMAP file
   ifstream in(vmap_file);
@@ -72,19 +135,27 @@ MapDB::import_vmap(const std::string & vmap_file, const Opt & opts){
     if (o.is_empty()) continue;
 
     MapDBObj o1;
-    if (o.type & 0x100000) o1.cl = MAPDB_LINE;
-    if (o.type & 0x200000) o1.cl = MAPDB_POLYGON;
+    int cl = o.type >> 20;
+    switch (cl){
+      case 0: o1.cl = MAPDB_POINT; break;
+      case 1: o1.cl = MAPDB_LINE; break;
+      case 2: o1.cl = MAPDB_POLYGON; break;
+      default:
+        throw Err() << "wrong VMAP object class: "<< cl;
+    }
 
     // convert type
+    o1.type = 0;
     for (auto const & cnv: cnvs[o1.cl]){
-      if (cnv.x == 0 || cnv.x == o.type & 0xFFFFF) {
-        o1.type = cnv.y? cnv.y : o.type & 0xFFFFF;
+      if (cnv.x == 0 || cnv.x == (o.type & 0xFFFFF)) {
+        o1.type = cnv.y? cnv.y : (o.type & 0xFFFFF);
         break;
       }
     }
 
     // skip unknown types
     if (!o1.type) continue;
+
 
     // name
     o1.name = o.text;
@@ -107,7 +178,7 @@ MapDB::import_vmap(const std::string & vmap_file, const Opt & opts){
     o1.dMultiLine::operator=(o);
 
     // add object
-    uint32_t id = add(o1);
+    add(o1);
   }
 
   // border
@@ -121,7 +192,7 @@ MapDB::import_vmap(const std::string & vmap_file, const Opt & opts){
 }
 
 /**********************************************************/
-/// Export objects to VMAP1 file.
+/// Export objects to VMAP file.
 void
 MapDB::export_vmap(const std::string & vmap_file, const Opt & opts){
 
@@ -140,30 +211,49 @@ MapDB::export_vmap(const std::string & vmap_file, const Opt & opts){
 
     int line_num[2] = {0,0};
     ifstream ff(opts.get<string>("config"));
-    while (1){
-      vector<string> vs = read_words(ff, line_num, true);
-      if (vs.size()<1) continue;
+    try {
+      while (1){
+        vector<string> vs = read_words(ff, line_num, true);
+        if (vs.size()<1) break;
 
-      int cl = -1;
-      if (vs[0]=="point" || vs[0]=="poi") cl=0;
-      if (vs[0]=="line" || vs[0]=="multiline") cl=1;
-      if (vs[0]=="polygon") cl=2;
+        if (vs[0]=="point"){
+          if (vs.size()!=2 && vs.size()!=3)
+            throw Err() << "point: two or three arguments expected: "
+                           "<type_in> [<type_out>]";
+          cnvs[0].push_back(iPoint(
+            str_to_type<int>(vs[1]), vs.size()<3? 0:str_to_type<uint16_t>(vs[2])));
+          continue;
+        }
 
-      if (cl>=0 &&  vs.size()>1 && vs.size()<3){
-        cnvs[cl].push_back(iPoint(
-          str_to_type<int>(vs[1]),
-          vs.size()<3? 0:str_to_type<int>(vs[2])));
-        continue;
+        if (vs[0]=="line"){
+          if (vs.size()!=2 && vs.size()!=3)
+            throw Err() << "line: two or three arguments expected: "
+                           "<type_in> [<type_out>]";
+          cnvs[1].push_back(iPoint(
+            str_to_type<int>(vs[1]), vs.size()<3? 0:str_to_type<uint16_t>(vs[2])));
+          continue;
+        }
+
+        if (vs[0]=="area"){
+          if (vs.size()!=2 && vs.size()!=3)
+            throw Err() << "area: two or three arguments expected: "
+                           "<type_in> [<type_out>]";
+          cnvs[2].push_back(iPoint(
+            str_to_type<int>(vs[1]), vs.size()<3? 0:str_to_type<uint16_t>(vs[2])));
+          continue;
+        }
+
+        throw Err() << "unknown command: " << vs[0];
       }
-
-      throw Err() << "bad configuration file at line "
-                  << line_num[0];
+    } catch (Err e){
+        throw Err() << "MapDB::export_mp: bad configuration file at line "
+                    << line_num[0] << ": " << e.str();
     }
   }
 
-  if (opts.exists("cnv_point"))   cnvs[0] = opts.get<dLine>("cnv_point");
-  if (opts.exists("cnv_line"))    cnvs[0] = opts.get<dLine>("cnv_line");
-  if (opts.exists("cnv_polygon")) cnvs[0] = opts.get<dLine>("cnv_polygon");
+  if (opts.exists("cnv_points")) cnvs[0] = opts.get<dLine>("cnv_points");
+  if (opts.exists("cnv_lines"))  cnvs[1] = opts.get<dLine>("cnv_lines");
+  if (opts.exists("cnv_areas"))  cnvs[2] = opts.get<dLine>("cnv_areas");
 
   VMap vmap_data;
   uint32_t key = 0;
@@ -174,6 +264,7 @@ MapDB::export_vmap(const std::string & vmap_file, const Opt & opts){
     o.unpack(str);
 
     VMapObj o1;
+    o1.type = 0;
 
     // convert type
     for (auto const & cnv: cnvs[o.cl]){
@@ -184,37 +275,44 @@ MapDB::export_vmap(const std::string & vmap_file, const Opt & opts){
     }
 
     // skip unknown types
-    if (!o1.type) continue;
+    if (o1.type != 0) {
 
-    if (o.cl == MAPDB_LINE)    o1.type |= 0x100000;
-    if (o.cl == MAPDB_POLYGON) o1.type |= 0x200000;
+      switch (o.cl){
+        case MAPDB_POINT:   break;
+        case MAPDB_LINE:    o1.type |= 0x100000; break;
+        case MAPDB_POLYGON: o1.type |= 0x200000; break;
+        default:
+          throw Err() << "wrong MapDB object class: "<< o.cl;
+      }
 
-    // name
-    o1.text = o.name;
+      // name
+      o1.text = o.name;
 
-    // comments
-    if (o.comm.size()){
-      int pos1=0, pos2=0;
-      do {
-        pos2 = o.comm.find('\n', pos1);
-        o1.comm.push_back(o.comm.substr(pos1,pos2));
-        pos1 = pos2+1;
-      } while (pos2!=string::npos);
+      // comments
+      if (o.comm.size()){
+        int pos1=0, pos2=0;
+        do {
+          pos2 = o.comm.find('\n', pos1);
+          o1.comm.push_back(o.comm.substr(pos1,pos2));
+          pos1 = pos2+1;
+        } while (pos2!=string::npos);
+      }
+
+      // direction
+      o1.dir = o.dir;
+
+      // source
+      if (o.tags.size()>0) o1.opts.put("Source", *o.tags.begin());
+
+      // angle (deg->deg)
+      if (o.angle!=0) o1.opts.put("Angle", o.angle);
+
+      // points
+      o1.dMultiLine::operator=(o);
+
+      vmap_data.push_back(o1);
     }
 
-    // direction
-    o1.dir = o.dir;
-
-    // source
-    if (o.tags.size()>0) o1.opts.put("Source", *o.tags.begin());
-
-    // angle (deg->deg)
-    if (o.angle!=0) o1.opts.put("Angle", o.angle);
-
-    // points
-    o1.dMultiLine::operator=(o);
-
-    vmap_data.push_back(o1);
     str = objects.get_next(key);
   }
 
