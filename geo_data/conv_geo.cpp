49cec36204f0278f321ebda4a2a6aa34d3c77fa1
Vladislav Zavjalov <slazav@altlinux.org>
2020-02-11 12:08:07 +0200

    geo_data/conv_geo: do not throw error on zero coordinates

--- geo_data/conv_geo.cpp
+++ geo_data/conv_geo.cpp
@@ -79,7 +79,7 @@ ConvGeo::frw_pt(dPoint & p) const{
     double *z = (cnv2d || isnan(p.z))? NULL:&p.z;
     if (pj_transform(pj_src.get(), pj_dst.get(), 1, 1, &p.x, &p.y, z)!=0)
       throw Err() << "Can't convert coordinates: " << pj_strerrno(pj_errno);
-    if (!isnormal(p.x) || !isnormal(p.y))
+    if (!isfinite(p.x) || !isfinite(p.y))
       throw Err() << "Can't convert coordinates: non-numeric result";
   }
   if (sc_dst!=1.0) {p.x*=sc_dst; p.y*=sc_dst;};
@@ -92,7 +92,7 @@ ConvGeo::bck_pt(dPoint & p) const{
     double *z = (cnv2d || isnan(p.z))? NULL:&p.z;
     if (pj_transform(pj_dst.get(), pj_src.get(), 1, 1, &p.x, &p.y, z)!=0)
       throw Err() << "Can't convert coordinates: " << pj_strerrno(pj_errno);
-    if (!isnormal(p.x) || !isnormal(p.y))
+    if (!isfinite(p.x) || !isfinite(p.y))
       throw Err() << "Can't convert coordinates: non-numeric result";
   }
   if (sc_src!=1.0) {p.x/=sc_src; p.y/=sc_src;}
