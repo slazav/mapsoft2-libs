bc3aac50b41a3a88e483b75b1d25470af8e0432b
Vladislav Zavjalov <slazav@altlinux.org>
2020-02-17 02:15:59 +0200

    mapdb: rewrite MapDBObj constructors

--- mapdb/mapdb.h
+++ mapdb/mapdb.h
@@ -45,7 +45,7 @@ struct MapDBObj: public dMultiLine {
   std::set<uint32_t> children;   // id's of related objects (usually labels)
 
   // defaults
-  MapDBObj() {type=0; angle=0;}
+  MapDBObj(const uint32_t t = 0): type(t), angle(0) {}
 
   // assemble object type:
   static uint32_t make_type(const uint16_t cl, const uint16_t tnum);
@@ -65,8 +65,8 @@ struct MapDBObj: public dMultiLine {
   // get object type number
   uint16_t get_tnum()  const;
 
-  MapDBObj(const uint16_t cl, const uint16_t tnum): MapDBObj() { set_type(cl,tnum); }
-  MapDBObj(const std::string & s): MapDBObj() { set_type(s); }
+  MapDBObj(const uint16_t cl, const uint16_t tnum): MapDBObj(make_type(cl,tnum)) {}
+  MapDBObj(const std::string & s): MapDBObj(make_type(s)) { }
 
   /***********************************************/
 
