940a01f9d1cfafd44380c33a292e8c12e370fead
Vladislav Zavjalov <slazav@altlinux.org>
2020-03-01 17:52:24 +0200

    mapdb/gobj_mapdb: fix border drawing

--- mapdb/gobj_mapdb.cpp
+++ mapdb/gobj_mapdb.cpp
@@ -839,7 +839,7 @@ GObjMapDB::DrawingStep::draw(const CairoWrapper & cr, const dRect & range){
   }
 
   // BRD drawing step:
-  if (action == STEP_DRAW_BRD && mapdb_gobj->ref.border.size() > 2) {
+  if (action == STEP_DRAW_BRD && mapdb_gobj->ref.border.size()>0) {
     cr->begin_new_path();
     cr->mkpath_smline(mapdb_gobj->ref.border, true, sm);
     // Pattern feature
