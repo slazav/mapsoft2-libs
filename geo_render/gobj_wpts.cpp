slazav/mapsoft2-libs

commit 6a9560b403896bd646732fd8d88a3f0d3b9c02a0

viewer, geo_render: change GObj interface: draw() return type; range var

geo_render/gobj_wpts.cpp
@@ -85,8 +85,8 @@ GObjWpts::set_cnv(const std::shared_ptr<ConvBase> cnv) {
     throw Err() << "GObjWpts: templates are not syncronized with data";
 
   for (auto & wt:tmpls) update_pt_crd(wt, cnv);
-  if (do_adj_pos) adjust_text_pos();
   update_range();
+  if (do_adj_pos) adjust_text_pos();
   redraw_me();
 }
 
@@ -100,7 +100,7 @@ GObjWpts::GObjWpts(GeoWptList & wpts): wpts(wpts) {
   }
 }
 
-int
+GObj::ret_t
 GObjWpts::draw(const CairoWrapper & cr, const dRect & draw_range) {
 
   if (is_stopped()) return GObj::FILL_NONE;
@@ -204,7 +204,7 @@ void
 GObjWpts::adjust_text_pos() {
   // create geohash storage
   GeoHashStorage db;
-  db.set_bbox(GObj::MAX_RANGE);
+  db.set_bbox(range);
 
   // for each box
   for (int i=0; i<tmpls.size(); ++i){

geo_render/gobj_maps.cpp
@@ -160,7 +160,7 @@ GObjMaps::prepare_range(const dRect & range) {
 }
 
 
-int
+GObj::ret_t
 GObjMaps::draw(const CairoWrapper & cr, const dRect & draw_range) {
 
   if (is_stopped()) return GObj::FILL_NONE;

geo_render/gobj_maps.h
@@ -24,6 +24,7 @@ private:
 
   // Original data. It may be edited through the GObj interface.
   GeoMapList & maps;
+  dRect range; // data range in viewer coords
 
   struct MapData{
     ConvMulti cnv;  // conversion from viewer coordinates to the map coordinates
@@ -96,7 +97,7 @@ public:
 
   void prepare_range(const dRect & range) override;
 
-  int draw(const CairoWrapper & cr, const dRect &box) override;
+  ret_t draw(const CairoWrapper & cr, const dRect &box) override;
 
 };
 

geo_render/gobj_pano.cpp
@@ -210,7 +210,7 @@ GObjPano::xy2geo(const iPoint & pt){
 
 /***********************************************************/
 
-int
+GObj::ret_t
 GObjPano::draw(const CairoWrapper & cr, const dRect &box){
   if (is_stopped()) return GObj::FILL_NONE;
   if (!srtm) return GObj::FILL_NONE;

geo_render/gobj_pano.h
@@ -98,7 +98,7 @@ public:
 
   void set_cnv(const std::shared_ptr<ConvBase> c) override;
 
-  int draw(const CairoWrapper & cr, const dRect &box) override;
+  ret_t draw(const CairoWrapper & cr, const dRect &box) override;
 
 };
 

geo_render/gobj_srtm.cpp
@@ -127,7 +127,7 @@ GObjSRTM::set_cnv(const std::shared_ptr<ConvBase> c) {
   redraw_me();
 }
 
-int
+GObj::ret_t
 GObjSRTM::draw(const CairoWrapper & cr, const dRect & draw_range) {
 
   if (!srtm) return GObj::FILL_NONE;

geo_render/gobj_srtm.h
@@ -58,9 +58,7 @@ class GObjSRTM : public GObj {
 
     void set_cnv(const std::shared_ptr<ConvBase> c) override;
 
-    int draw(const CairoWrapper & cr, const dRect & draw_range) override;
-
-
+    ret_t draw(const CairoWrapper & cr, const dRect & draw_range) override;
 
 };
 

geo_render/gobj_trk.cpp
@@ -131,7 +131,7 @@ GObjTrk::GObjTrk(GeoTrk & trk_): trk(trk_){
   segments.resize(trk.size());
 }
 
-int
+GObj::ret_t
 GObjTrk::draw(const CairoWrapper & cr, const dRect & draw_range){
 
   if (is_stopped()) return GObj::FILL_NONE;

commit 8b469cbadde9dead2481ec884aabd5149a3ab2b0

geo_render/gobj_wpts: fix bbox calculation broken in 6a9560b403896bd646732fd8d88a3f0d3b9c02a0

geo_render/gobj_wpts.cpp
@@ -85,8 +85,8 @@ GObjWpts::set_cnv(const std::shared_ptr<ConvBase> cnv) {
     throw Err() << "GObjWpts: templates are not syncronized with data";
 
   for (auto & wt:tmpls) update_pt_crd(wt, cnv);
-  update_range();
   if (do_adj_pos) adjust_text_pos();
+  update_range();
   redraw_me();
 }
 