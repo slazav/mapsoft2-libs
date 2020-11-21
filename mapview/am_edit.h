e22f01a0b6d02556bc72f90a563d53a83dd620bb
Vladislav Zavjalov <slazav@altlinux.org>
2020-11-21 12:30:57 +0000

    mapview/am_edit.h: adding track segments

--- mapview/am_edit.h
+++ mapview/am_edit.h
@@ -3,11 +3,39 @@
 
 #include "am.h"
 
+/* Edit Geodata mode.
+
+default regime -> click on waypoint, track point or track interval (visible line between points):
+* Left button: start moving waypoint or trackpoint, or add point to the interval.
+* Ctrl-left button: delete waypoint, trackpoint, or splin track at the given interval.
+* right button: show waypoint, track point, or track interval menu.
+
+Waypoint menu:
+* move waypoint (same as left click on waypoint)
+* delete waypoint (same as ctrl-left click on waypoint)
+
+Trackpoint menu:
+* move track point (same as left click on the point)
+* delete track point (same as ctrl-left click on the point)
+* add segment: add new segment to the track
+* delete segment: delete track segment which contains this point
+* delete track
+
+Track interval menu:
+* add trackpoint (same as left click on the interval)
+* split track (same as ctrl-left click on the interval)
+* add segment: add new segment to the track
+* delete segment: delete track segment which contains this interval
+* delete track
+
+*/
+
 class AMEditData : public ActionMode {
 int mystate;
 PanelTrks::ptr_t trk;
 PanelWpts::ptr_t wpts;
 size_t   idx;
+dLine pts; // points for adding track parts
 
 Glib::RefPtr<Gtk::ActionGroup> actions;
 Glib::RefPtr<Gtk::UIManager> ui_manager;
@@ -37,17 +65,21 @@ public:
       sigc::mem_fun(this, &AMEditData::del_wpt));
 
     actions->add(
-      Gtk::Action::create("EditData:tseg:add", "Add trackpoint", ""),
+      Gtk::Action::create("EditData:tseg:addpt", "Add trackpoint", ""),
       sigc::mem_fun(this, &AMEditData::add_tpt_start));
     actions->add(
       Gtk::Action::create("EditData:tseg:split", "Split track", ""),
       sigc::mem_fun(this, &AMEditData::split_trk));
+
     actions->add(
-      Gtk::Action::create("EditData:tseg:del", "Delete segment", ""),
+      Gtk::Action::create("EditData:delseg", "Delete segment", ""),
       sigc::mem_fun(this, &AMEditData::del_trkseg));
     actions->add(
       Gtk::Action::create("EditData:deltrk", "Delete track", ""),
       sigc::mem_fun(this, &AMEditData::del_trk));
+    actions->add(
+      Gtk::Action::create("EditData:addseg", "Add new segment", ""),
+      sigc::mem_fun(this, &AMEditData::add_trkseg_start));
 
 
     ui_manager->add_ui_from_string(
@@ -59,13 +91,15 @@ public:
       "  <popup name='EditData:tpt'>"
       "    <menuitem action='EditData:tpt:move'/>"
       "    <menuitem action='EditData:tpt:del'/>"
-      "    <menuitem action='EditData:tseg:del'/>"
+      "    <menuitem action='EditData:addseg'/>"
+      "    <menuitem action='EditData:delseg'/>"
       "    <menuitem action='EditData:deltrk'/>"
       "  </popup>"
       "  <popup name='EditData:tseg'>"
-      "    <menuitem action='EditData:tseg:add'/>"
+      "    <menuitem action='EditData:tseg:addpt'/>"
       "    <menuitem action='EditData:tseg:split'/>"
-      "    <menuitem action='EditData:tseg:del'/>"
+      "    <menuitem action='EditData:addseg'/>"
+      "    <menuitem action='EditData:delseg'/>"
       "    <menuitem action='EditData:deltrk'/>"
       "  </popup>"
       "</ui>"
@@ -79,15 +113,12 @@ public:
 
   void handle_click(const iPoint p, const int button,
                     const Gdk::ModifierType & state) override {
-    if (button == 3 && mystate != 0) {
-      abort();
-      return;
-    }
     switch (mystate){
       case 0: find_object(p, button, state); break;
-      case 1: move_wpt_finish(p); break;
-      case 2: move_tpt_finish(p); break;
-      case 3: add_tpt_finish(p); break;
+      case 1: move_wpt_finish(p, button); break;
+      case 2: move_tpt_finish(p, button); break;
+      case 3: add_tpt_finish(p, button); break;
+      case 4: add_trkseg_finish(p, button, state); break;
     }
   }
 
@@ -98,7 +129,8 @@ public:
 
 
 private:
-  void find_object(const iPoint p, const int button, const Gdk::ModifierType & state){
+  void find_object(const iPoint p, const int button,
+                   const Gdk::ModifierType & state){
 
     // find waypoints
     auto res1 = mapview->panel_wpts->find_points(p);
@@ -155,10 +187,9 @@ private:
     mystate=1;
   }
 
-  void move_wpt_finish(iPoint p){
-    wpts->set_point_crd(idx, p);
-    mapview->rubber.clear();
-    mystate=0;
+  void move_wpt_finish(const iPoint p, const int button){
+    if (button != 3) wpts->set_point_crd(idx, p);
+    abort();
   }
 
   void move_tpt_start() {
@@ -180,10 +211,9 @@ private:
     mystate=2;
   }
 
-  void move_tpt_finish(iPoint p){
-    trk->set_point_crd(idx, p);
-    mapview->rubber.clear();
-    mystate=0;
+  void move_tpt_finish(const iPoint p, const int button){
+    if (button != 3) trk->set_point_crd(idx, p);
+    abort();
   }
 
   void add_tpt_start() {
@@ -198,10 +228,9 @@ private:
     mystate=3;
   }
 
-  void add_tpt_finish(iPoint p){
-    trk->add_point_crd(idx, p);
-    mapview->rubber.clear();
-    mystate=0;
+  void add_tpt_finish(const iPoint p, const int button){
+    if (button != 3) trk->add_point_crd(idx, p);
+    abort();
   }
 
   void del_tpt()    { trk->del_point(idx); }
@@ -209,6 +238,57 @@ private:
   void split_trk()  { trk->split_trk(idx); }
   void del_trkseg() { trk->del_seg(idx); }
   void del_trk()    { mapview->panel_trks->remove(trk); }
+
+  void add_trkseg_start() {
+    pts.clear();
+    mystate = 4;
+    mapview->spanel.message("Add new segment to the track "
+      "(left click: add point, ctrl-left: remove last point, right: finish, ctrl-right: abort)");
+  }
+
+  void add_trkseg_finish(const iPoint p, const int button,
+                         const Gdk::ModifierType & state) {
+
+    if (button == 3) {
+       if (! (state&Gdk::CONTROL_MASK)) trk->add_segment_crd(pts);
+       if (pts.size()==0) {
+         mystate = 0;
+         mapview->spanel.message(get_name());
+       }
+       pts.clear();
+       mapview->rubber.clear();
+       return;
+    }
+
+    // remove point
+    if (button == 1 && state&Gdk::CONTROL_MASK){
+      if (pts.size()>0) pts.pop_back();
+      if (mapview->rubber.size()>0){
+        mapview->rubber.pop();
+      }
+      if (mapview->rubber.size()>0){
+        RubberSegment s = mapview->rubber.pop();
+        s.flags |= RUBBFL_MOUSE_P2;
+        s.p2=iPoint(0,0);
+        mapview->rubber.add(s);
+      }
+      return;
+    }
+
+    // add point
+    if (button == 1){
+      pts.push_back(dPoint(p));
+
+      if (mapview->rubber.size()>0){
+        RubberSegment s = mapview->rubber.pop();
+        s.flags &= ~RUBBFL_MOUSE;
+        s.p2 = dPoint(p);
+        mapview->rubber.add(s);
+      }
+      mapview->rubber.add_line(p);
+      return;
+    }
+  }
 
 };
 
