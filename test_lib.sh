4112d3935a6c30b5b69be6e0c53a39e5181eba65
Vladislav Zavjalov <slazav@altlinux.org>
2021-03-03 21:22:12 +0000

    test_lib.sh: add assert_cmd_substr command

--- test_lib.sh
+++ test_lib.sh
@@ -1,4 +1,4 @@
-# Run command and check its output and status code.
+# Run command and check its output (full match) and status code.
 assert_cmd(){
   cmd="$1"
   exp="$2"
@@ -11,6 +11,32 @@ assert_cmd(){
   if [ "$exp" != "$res" ]; then
     printf "ERROR ($cmd):\n"
     printf "  exp:\n%s\n" "$exp"
+    printf "  res:\n%s\n" "$res"
+    exit 1
+  fi
+  if [ "$rete" != "" -a "$ret" != "$rete" ]; then
+    printf "ERROR in return code ($cmd):\n"
+    printf "  exp:\n%s\n" "$rete"
+    printf "  ret:\n%s\n" "$ret"
+    exit 1
+  fi
+}
+
+# Run command and check its output (substring) and status code.
+assert_cmd_substr(){
+  cmd="$1"
+  exp="$2"
+  set +o errexit
+  res="$($cmd 2>&1)";
+  ret="$?"
+  rete="${3:-''}"
+  printf "%s" "$res" | grep -q "$exp"
+  retg="$?"
+  set -o errexit
+
+  if [ "$retg" != 0 ]; then
+    printf "ERROR ($cmd):\n"
+    printf "  exp substring:\n%s\n" "$exp"
     printf "  res:\n%s\n" "$res"
     exit 1
   fi
