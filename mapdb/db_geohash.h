00e03a094bb69e8033b9d4a4aab9fb96e8ef90a5
Vladislav Zavjalov <slazav@altlinux.org>
2022-09-11 21:45:27 +0100

    mapdb: Memory implementation for GeoHashDB; fix GeoHashDB::bbox; add GeoHashDB::dump

--- mapdb/db_geohash.h
+++ mapdb/db_geohash.h
@@ -6,38 +6,49 @@
 #include <stdint.h>
 #include "geom/rect.h"
 
-// Berkleydb-based geohash database for spatial indexing.
+// Geohash database for spatial indexing.
 // One can add "object ID -- object type -- object range"
 // combinations and then query for objects of a certain type
 // which may appear in a given coordinate range.
 
+// There are two implementations: Memory and BerkleyDB.
+// Use empty fname parameter in the constructor to use memory implementation.
+
+// Memory version is almost same as GeoHashStorage class in geohash/
+// (TODO: remove one)
+
 class GeoHashDB {
   private:
     class Impl;
-    std::unique_ptr<Impl> impl;
+    class ImplDB;
+    class ImplMem;
+    std::shared_ptr<Impl> impl;
 
   public:
 
-   GeoHashDB(std::string fname, const char *dbname, bool create);
-   ~GeoHashDB();
+    GeoHashDB(std::string fname = std::string(), const char *dbname = NULL, bool create = true);
+    ~GeoHashDB();
 
-   // Get id of objects of certain type which may be found in the range.
-   // If bbox is empty return zero set.
-   std::set<uint32_t> get(const uint32_t type, const dRect & range);
+    // Get id of objects of certain type which may be found in the range.
+    // If bbox is empty return zero set.
+    std::set<uint32_t> get(const uint32_t type, const dRect & range);
 
-   // Add an object with id, type and range.
-   // If bbox is empty do nothing.
-   void put(const uint32_t id, const uint32_t type, const dRect & range);
+    // Add an object with id, type and range.
+    // If bbox is empty do nothing.
+    void put(const uint32_t id, const uint32_t type, const dRect & range);
 
-   // Delete an object with id, type and range.
-   // If the record does not exist do nothing.
-   void del(const uint32_t id, const uint32_t type, const dRect & range);
+    // Delete an object with id, type and range.
+    // If the record does not exist do nothing.
+    void del(const uint32_t id, const uint32_t type, const dRect & range);
 
-   // get all object types in the database
-   std::set<uint32_t> get_types();
+    // get all object types in the database
+    std::set<uint32_t> get_types();
 
     // get range of the largest geohash
     dRect bbox();
+
+    // dump database to stdout
+    void dump();
 
 };
 
---
+++ mapdb/db_geohash_mem.test.cpp
@@ -0,0 +1,105 @@
+///\cond HIDDEN (do not show this in Doxyden)
+
+#include <cassert>
+#include <unistd.h>
+#include <iostream>
+#include "err/assert_err.h"
+#include "db_geohash.h"
+
+int
+main(){
+  try{
+      GeoHashDB db;
+
+      assert_eq(db.get_types().size(), 0);
+      assert_eq(db.bbox(), dRect());
+
+      db.put(1, 0, dRect(-0.01,-0.01, 0.02,0.02));
+      db.put(2, 0, dRect(1,-0.01,     0.02,0.02));
+      db.put(3, 0, dRect(-0.01,1,     0.02,0.02));
+      db.put(4, 0, dRect(1,1,         0.01,0.01));
+      assert_eq(type_to_str(db.bbox()), "[-0.0439453125,-0.0439453125,1.09863281,1.09863281]");
+      db.put(5, 0, dRect(36,57,       0.01,0.01));
+      assert_eq(type_to_str(db.bbox()), "[-0.0439453125,-0.0439453125,36.0791016,57.0849609]");
+      db.put(6, 0, dRect(36,57,       0.11,0.11));
+      db.put(7, 0, dRect(35,57,       0.11,0.11));
+      db.put(8, 0, dRect(35,57,       0.10,0.10));
+      db.put(9, 0, dRect(-100,-50,    220,100));
+      db.put(10, 0, dRect()); // empty range - do nothing
+      db.put(11, 0, dRect(36,57,0,0));
+      assert_eq(type_to_str(db.bbox()), "[-180,-90,360,180]");
+      std::set<uint32_t> v1 = db.get(0, dRect());
+      assert_eq(v1.size(),0);
+
+      assert_eq(db.get_types().size(), 1);
+      v1 = db.get_types();
+      assert_eq(v1.size(), 1);
+      assert_eq(v1.count(0),1);
+
+
+      v1 = db.get(0, dRect(-0.01,-0.01, 1.12,1.12));
+      //for (auto i:v1) std::cerr << "> " << i << "\n";
+      assert_eq(v1.size(),5);
+      assert_eq(v1.count(1),1);
+      assert_eq(v1.count(2),1);
+      assert_eq(v1.count(3),1);
+      assert_eq(v1.count(4),1);
+      assert_eq(v1.count(9),1);
+
+      v1 = db.get(1, dRect(-0.01,-0.01, 1.12,1.12));
+      assert_eq(v1.size(),0);
+
+      v1 = db.get(0, dRect(36,57, 0.001,0.001));
+      //for (auto i:v1) std::cerr << "> " << i << "\n";
+      assert_eq(v1.size(),4);
+      assert_eq(v1.count(5),1);
+      assert_eq(v1.count(6),1);
+      assert_eq(v1.count(9),1);
+      assert_eq(v1.count(11),1);
+
+      v1 = db.get(0, dRect(-180,-90, 360,180));
+      //for (auto i:v1) std::cerr << "> " << i << "\n";
+      assert_eq(v1.size(),10);
+
+      db.del(9, 0, dRect(-100,-50, 220,100));
+      db.del(5, 1, dRect(-100,-50, 220,100));
+      db.put(9, 1, dRect(-100,-50, 220,100));
+      //for (auto i:v1) std::cerr << "> " << i << "\n";
+      v1 = db.get(0, dRect(36,57, 0.001,0.001));
+      assert_eq(v1.size(),3);
+      assert_eq(v1.count(5),1);
+      assert_eq(v1.count(6),1);
+      assert_eq(v1.count(11),1);
+
+      v1 = db.get(1, dRect(36,57, 0.001,0.001));
+      assert_eq(v1.size(),1);
+      assert_eq(v1.count(9),1);
+
+      db.del(5, 0, dRect(36,57,0.01,0.01));
+      v1 = db.get(0, dRect(36,57, 0.001,0.001));
+      assert_eq(v1.size(),2);
+      assert_eq(v1.count(6),1);
+      assert_eq(v1.count(11),1);
+
+      // non-existing id-type-range combinations!
+      db.del(6, 0, dRect(36,57,0.01,0.01));
+      db.del(6, 1, dRect(36,57,0.11,0.11));
+      v1 = db.get(0, dRect(36,57, 0.001,0.001));
+      assert_eq(v1.size(),2);
+      assert_eq(v1.count(6),1);
+      assert_eq(v1.count(11),1);
+
+      v1 = db.get_types();
+      assert_eq(v1.size(), 2);
+      assert_eq(v1.count(0),1);
+      assert_eq(v1.count(1),1);
+
+  }
+  catch (Err & e) {
+    std::cerr << "Error: " << e.str() << "\n";
+    return 1;
+  }
+  return 0;
+}
+
+///\endcond
