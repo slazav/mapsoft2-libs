b0788ab6a67a9c2dce74c9a03e23c761db2c5d99
Vladislav Zavjalov <slazav@altlinux.org>
2024-03-30 08:02:08 +0000

    srtm.h: format code

--- srtm/srtm.h
+++ srtm/srtm.h
@@ -84,61 +84,55 @@ class SRTM {
     std::unique_lock<std::mutex> get_lock() {
       return std::unique_lock<std::mutex>(cache_mutex);}
 
-/******************************/
-// new interface
+    /******************************/
+    // new interface
 
-typedef enum {
-  SRTM_NEAREST,
-  SRTM_LINEAR,
-  SRTM_CUBIC,
-  SRTM_SMOOTH,
-} style_t;
+    typedef enum {
+      SRTM_NEAREST,
+      SRTM_LINEAR,
+      SRTM_CUBIC,
+      SRTM_SMOOTH,
+    } style_t;
 
-style_t srtm_interp;
-double  srtm_smooth_rad;
+    style_t srtm_interp;
+    double  srtm_smooth_rad;
 
-// Distance between points (dx,dy) at a given place.
-// (0,0) if data is missing.
-dPoint get_step(const iPoint& p);
+    // Distance between points (dx,dy) at a given place.
+    // (0,0) if data is missing.
+    dPoint get_step(const iPoint& p);
 
-// Low-level get function: rounding coordinate to the nearest point
-int16_t get_raw(const dPoint& p);
+    // Low-level get function: rounding coordinate to the nearest point
+    int16_t get_raw(const dPoint& p);
 
-// get with interpolation/smoothing
-int16_t get_h(const dPoint& p);
+    // get with interpolation/smoothing
+    int16_t get_h(const dPoint& p);
 
-// get slope
-double get_s(const dPoint& p);
+    // get slope
+    double get_s(const dPoint& p);
 
-/******************************/
-// color surface interface
+    /******************************/
+    // color surface interface
 
-enum draw_mode_t {
-  SRTM_DRAW_SHADES, // heights shaded with slope value
-  SRTM_DRAW_HEIGHTS,
-  SRTM_DRAW_SLOPES,
-} draw_mode;
+    enum draw_mode_t {
+      SRTM_DRAW_SHADES, // heights shaded with slope value
+      SRTM_DRAW_HEIGHTS,
+      SRTM_DRAW_SLOPES,
+    } draw_mode;
 
-double hmin,hmax;  // limits for heights and shades modes
-double smin,smax;  // limits for slopes mode
-uint32_t bgcolor;  // how to draw holes
-Rainbow R; // color converter
+    double hmin,hmax;  // limits for heights and shades modes
+    double smin,smax;  // limits for slopes mode
+    uint32_t bgcolor;  // how to draw holes
+    Rainbow R; // color converter
 
-// Get color for given height and slope, according with drawing options
-uint32_t get_color(const double h, const double s);
+    // Get color for given height and slope, according with drawing options
+    uint32_t get_color(const double h, const double s);
 
-/// Get color for a point (lon-lat coords), according with drawing options.
-uint32_t get_color(const dPoint & p);
+    /// Get color for a point (lon-lat coords), according with drawing options.
+    uint32_t get_color(const dPoint & p);
 
-uint32_t get_bgcolor() const {return bgcolor;}
+    uint32_t get_bgcolor() const {return bgcolor;}
 
-/******************************/
-
-    // Find set of points with same value (used
-    // for hole interpolation in get_val) and its border.
-    // `max` is max.set size (default is 0 for no limits).
-//    void plane_and_border(const iPoint& p,
-//       std::set<iPoint>& set, std::set<iPoint>& brd, size_t max=0);
+    /******************************/
 
     // make vector data: contours
     // use kx parameter to use only every kx-th horizontal point.
