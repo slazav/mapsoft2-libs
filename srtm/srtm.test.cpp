724f96e7568073151a9bc0ba4bbae7c6a99b5696
Vladislav Zavjalov <slazav@altlinux.org>
2024-03-30 11:29:43 +0000

    srtm: fix test

--- srtm/srtm.test.cpp
+++ srtm/srtm.test.cpp
@@ -17,20 +17,20 @@ main(){
     assert_eq(S.get_h(dPoint(-1.0,-1.0)), SRTM_VAL_NOFILE);
     assert_eq(S.get_h(dPoint(29.0,77.0)), SRTM_VAL_NOFILE);
 
-    assert_eq(S.get_h(dPoint(29.1, 78.1)), 0);
-    assert_eq(S.get_h(dPoint(29.6, 78.9)), 58);
+    assert_feq(S.get_h(dPoint(29.1, 78.1)), 0, 1e-3);
+    assert_feq(S.get_h(dPoint(29.6, 78.9)), 58, 1e-3);
 
     o.put("srtm_interp", "linear");
     S.set_opt(o);
 
-    assert_eq(S.get_h(dPoint(29.1, 78.1)), 0);
-    assert_eq(S.get_h(dPoint(29.6, 78.9)), 57);
+    assert_feq(S.get_h(dPoint(29.1, 78.1)), 0, 1e-3);
+    assert_feq(S.get_h(dPoint(29.6, 78.9)), 58, 1e-3);
 
     o.put("srtm_interp", "cubic");
     S.set_opt(o);
 
-    assert_eq(S.get_h(dPoint(29.1, 78.1)), 0);
-    assert_eq(S.get_h(dPoint(29.6, 78.9)), 57);
+    assert_feq(S.get_h(dPoint(29.1, 78.1)), 0, 1e-3);
+    assert_feq(S.get_h(dPoint(29.6, 78.9)), 58, 1e-3);
 
     Opt o1 = S.get_def_opt();
     assert_eq(o1.exists("srtm_dir"), true);
