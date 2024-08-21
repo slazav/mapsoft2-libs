4967825a5d6e14d68e6c51a1abbc368b4748d853
Vladislav Zavjalov <slazav@altlinux.org>
2024-08-21 07:51:37 +0300

    srtm: update tests: SRTMTile and coordinate conversions

--- srtm/srtm.test.cpp
+++ srtm/srtm.test.cpp
@@ -8,6 +8,69 @@ int
 main(){
   try{
 
+
+    // tiles
+    {
+      // empty tile (ALOS by default)
+      SRTMTile T("./test_srtm", iPoint(30.2, 78.0));
+      assert_eq(T.is_empty(), true);
+      assert_eq(T.srtm, false);
+      assert_eq(T.w, 0);
+      assert_eq(T.h, 0);
+      assert_eq(T.key, iPoint(30,78));
+      assert_eq(T.step, dPoint());
+    }
+    {
+      // non-empty SRTM tile
+      SRTMTile T("./test_srtm", iPoint(29.1, 78.2));
+      assert_eq(T.is_empty(), false);
+      assert_eq(T.srtm, true);
+      assert_eq(T.w, 1201);
+      assert_eq(T.h, 1201);
+      assert_eq(T.key, iPoint(29,78));
+      assert_deq(T.step, dPoint(1.0,1.0)/1200.0, 1e-8);
+    }
+
+    // coordinate conversions
+    {
+      iPoint key(10,10);
+      SRTMTile T("./test_srtm", key);
+      assert_eq(T.is_empty(), true);
+      assert_eq(T.key, key);
+
+      // check SRTM
+      T.srtm = 1;
+      T.w = T.h = 1201;
+      T.step = dPoint(1.0,1.0)/1200.0;
+
+      // check all 4 corners
+      assert_deq(T.px2ll(dPoint(0,0)),       dPoint(0,1) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(1200,0)),    dPoint(1,1) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(0,1200)),    dPoint(0,0) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(1200,1200)), dPoint(1,0) + key, 1e-8);
+
+      // check step
+      assert_deq(T.px2ll(dPoint(1,1)),       dPoint(T.step.x, 1-T.step.y) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(1199,1199)), dPoint(1-T.step.x, T.step.y) + key, 1e-8);
+
+      // switch to ALOS
+      T.srtm = 0;
+      T.w = T.h = 1200;
+      T.step = dPoint(1.0,1.0)/1200.0;
+
+      // check all 4 corners
+      double dx = 0.5*T.step.x, dy = 0.5*T.step.y;
+      assert_deq(T.px2ll(dPoint(0,0)),       dPoint(  dx, 1-dy) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(1199,0)),    dPoint(1-dx, 1-dy) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(0,1199)),    dPoint(  dx,   dy) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(1199,1199)), dPoint(1-dx,   dy) + key, 1e-8);
+
+      // check step
+      assert_deq(T.px2ll(dPoint(1,1)),       dPoint(  3*dx, 1-3*dy) + key, 1e-8);
+      assert_deq(T.px2ll(dPoint(1198,1198)), dPoint(1-3*dx,   3*dy) + key, 1e-8);
+
+    }
+
     Opt o;
     o.put("srtm_dir", "./test_srtm");
     o.put("srtm_interp", "nearest");
