slazav/mapsoft2-libs

commit e95e54b23b3eef27ca4ff6127afe8a2d9b9a582b

geo_render/gobj_maps: fix error with coordinate checking

geo_render/gobj_maps.cpp
@@ -181,6 +181,7 @@ GObjMaps::render_tile(const dRect & draw_range) {
     if (!d.src->is_tiled && draw_map)
       imageR_src = img_cache.get(d.src->image, d.load_sc);
 
+    if (imageR_src.is_empty()) continue;
     double avr = d.scale/d.load_sc;
 
     // Simplify map-to-map conversion if possible.
@@ -200,7 +201,7 @@ GObjMaps::render_tile(const dRect & draw_range) {
         if (d.brd.size() && !dPolyTester::test_cr(cr, p.x)) continue;
 
         cnv.frw(p); // convert to source image coordinates
-        image_src->check_crd(p.x, p.y);
+        if (!image_src->check_crd(p.x, p.y)) continue;
 
         int color;
         if (smooth){

commit 6c9815f760725b1f6262c0edb440ad7b040891ad

geo_render/gobj_maps: fix error with tiled maps introduced in e95e54b23b3eef27ca4ff6127afe8a2d9b9a582b

geo_render/gobj_maps.cpp
@@ -172,16 +172,16 @@ GObjMaps::render_tile(const dRect & draw_range) {
 
     bool draw_map = d.scale*pow(2,d.zoom) >= minsc &&
                     d.scale*pow(2,d.zoom) <= maxsc;
-
     // tiled map
     if (d.src->is_tiled && draw_map)
       image_src = d.timg.get();
 
     // non-tiled maps
-    if (!d.src->is_tiled && draw_map)
+    if (!d.src->is_tiled && draw_map){
       imageR_src = img_cache.get(d.src->image, d.load_sc);
+      if (imageR_src.is_empty()) continue;
+    }
 
-    if (imageR_src.is_empty()) continue;
     double avr = d.scale/d.load_sc;
 
     // Simplify map-to-map conversion if possible.