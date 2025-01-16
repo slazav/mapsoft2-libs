d2737708dfa67c0026a14fd2195b1016cb94b5c3
Vladislav Zavjalov <slazav@altlinux.org>
2025-01-16 10:14:49 +0200

    conv_geo: fix aliases for NO systems; add one for SE

--- geo_data/conv_geo.cpp
+++ geo_data/conv_geo.cpp
@@ -30,6 +30,9 @@ std::string expand_proj_aliases(const std::string & pars){
   if (pars == "ESPG:25835") // Norway, zone 35 (EPSG:25835)
     return "+proj=utm +zone=35 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
 
+  if (pars == "ESPG:3006") // Sweeden
+    return "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs";
+
   if (pars == "GB") // Breat Britain maps (EPSG:27700)
     return "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717"
            " +x_0=400000 +y_0=-100000 +datum=OSGB36 +units=m +no_defs";
